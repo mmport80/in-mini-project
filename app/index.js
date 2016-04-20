52789eec534a09a8c2282cffa38a14a40222f47a
John Orford <John.Orford@gmail.com>
2016-04-20 19:36:56 +0200

    sundry

--- app/index.js
+++ app/index.js
@@ -1,1 +1,37 @@
+import "machine_learning";
+
 console.log("xoxo");
+const a = x => 1;
+
+
+var ml = require('machine_learning');
+
+var data =[['slashdot','USA','yes',18],
+           ['google','France','yes',23],
+           ['digg','USA','yes',24],
+           ['kiwitobes','France','yes',23],
+           ['google','UK','no',21],
+           ['(direct)','New Zealand','no',12],
+           ['(direct)','UK','no',21],
+           ['google','USA','no',24],
+           ['slashdot','France','yes',19],
+           ['digg','USA','no',18,],
+           ['google','UK','no',18,],
+           ['kiwitobes','UK','no',19],
+           ['digg','New Zealand','yes',12],
+           ['slashdot','UK','no',21],
+           ['google','UK','yes',18],
+           ['kiwitobes','France','yes',19]];
+var result = ['None','Premium','Basic','Basic','Premium','None','Basic','Premium','None','None','None','None','Basic','None','Basic','Basic'];
+
+var dt = new ml.DecisionTree({
+    data : data,
+    result : result
+});
+
+dt.build();
+
+console.log("Classify : ", dt.classify( ['xo','USA','no',21] ) );
+
+dt.prune(0.1); // 1.0 : mingain.
+console.log(dt);
